2d19a47f539fb6cce2174371da72e96b4b7ecc65
Robert Romero <21332@columbushs.com>
2019-10-28 09:37:30 -0400

    unfinished

--- src/candymachine/CandyMachine.java
+++ src/candymachine/CandyMachine.java
@@ -1,21 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package candymachine;
-
+import java.util.Scanner;
 /**
  *
  * @author mlarrubia
  */
 public class CandyMachine {
-
+    private static Scanner kb = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("How much money do you have?");
+        double money = kb.nextDouble();
+            candyMachine();
+        if (money > .50){
+            candyMachine();
+        }
+        else{
+            System.out.println("Get more money!");
+        }
+        
+        
+    }
+    public static void candyMachine () {
+        
+        
+        System.out.println("what would you like to have");
+        String choice = kb.nextLine();
+        double cost;
+//        double twix = 0.65;
+//        double chips = 0.50;
+//        double nutterButter = 0.75;
+//        double peanutButterCup = 0.65;
+//        double juicyFruitGum = 0.55;
+        
+        switch (choice) {
+            case "Twix":
+                cost = 0.65;
+                 break;
+            case "chips":
+                cost = 0.50;
+                 break;
+            case "nutter butter":
+                cost = 0.75;
+                 break;
+            case "peanut butter cup":
+                cost = 0.65;
+                break;
+            case "juicy fruit gum":
+                cost = 0.55;
+                break;
+            default:
+                cost = 0;
+                break;
+        }
+        if (cost > 0){
+            System.out.println("You have purchased " + choice + " for $" +cost);
+        }
+        else{
+            System.out.println("you haven't selected a candy that is in stock");
+        }
+        
     }
     
 }
