43ce7ee886e5041e37c1c8b708aeaf5cb3c97f60
rromero123102 <rromero21332@columbushs.com>
2019-10-28 21:41:06 -0400

    done

--- src/candymachine/CandyMachine.java
+++ src/candymachine/CandyMachine.java
@@ -10,57 +10,61 @@ public class CandyMachine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("How much money do you have?");
-        double money = kb.nextDouble();
-            candyMachine();
-        if (money > .50){
-            candyMachine();
-        }
-        else{
-            System.out.println("Get more money!");
-        }
+        candyMachine();
+        
         
         
     }
-    public static void candyMachine () {
+    public static void candyMachine() {
+        System.out.println("How much money you got?");
+        double money = kb.nextDouble();
+        if (money > .49){
+            System.out.println("what would you like to have");
+            System.out.println("twix \n"
+                    + "chips \n"
+                    + "nutter butter \n"
+                    + "peanut butter cup \n"
+                    + "juicy fruit gum?");
+            double cost;
+            System.out.println("***CASE SENSITIVE***");
+            // choice1 is unused, and is there to fix bug with scanner class
+            String choice1 = kb.nextLine();
+            String choice = kb.nextLine();
         
-        
-        System.out.println("what would you like to have");
-        String choice = kb.nextLine();
-        double cost;
-//        double twix = 0.65;
-//        double chips = 0.50;
-//        double nutterButter = 0.75;
-//        double peanutButterCup = 0.65;
-//        double juicyFruitGum = 0.55;
-        
-        switch (choice) {
-            case "Twix":
+            switch (choice) {
+               case "twix":
                 cost = 0.65;
-                 break;
-            case "chips":
+                    break;
+               case "chips":
                 cost = 0.50;
-                 break;
-            case "nutter butter":
+                    break;
+               case "nutter butter":
                 cost = 0.75;
-                 break;
-            case "peanut butter cup":
+                    break;
+               case "peanut butter cup":
                 cost = 0.65;
-                break;
-            case "juicy fruit gum":
+                    break;
+               case "juicy fruit gum":
                 cost = 0.55;
-                break;
-            default:
+                    break;
+               default:
                 cost = 0;
-                break;
-        }
-        if (cost > 0){
-            System.out.println("You have purchased " + choice + " for $" +cost);
-        }
+                    break;
+            }
+            double change = money - cost;
+            if (cost > 0){
+               System.out.println("You have purchased " + choice + " for $" + cost);
+            }
+            else{
+              System.out.println("you haven't selected a candy that is in stock");
+            }
+            
+            System.out.println("Your change is " + change);
+        }      
         else{
-            System.out.println("you haven't selected a candy that is in stock");
+            System.out.println("Get more money!");
+            
         }
-        
     }
     
 }
